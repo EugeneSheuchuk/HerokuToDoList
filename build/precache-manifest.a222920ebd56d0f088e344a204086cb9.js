b0323cbf032a476ac0986bda4c9d7522c9852e26
Yauheni Sheuchuk <shevchuked@tut.by>
2020-04-02 17:45:55 +0300

    add source

---
+++ build/precache-manifest.a222920ebd56d0f088e344a204086cb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6f4e9e869750e6a6f8b61c6f594a7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67ea7b4da9854405d2e5",
+    "url": "/static/css/main.6d3555a5.chunk.css"
+  },
+  {
+    "revision": "a0ab59c720b4b453c36c",
+    "url": "/static/js/2.ec8ebb47.chunk.js"
+  },
+  {
+    "revision": "c346956c1449420dcbe7f2a51cdfc9c4",
+    "url": "/static/js/2.ec8ebb47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ea7b4da9854405d2e5",
+    "url": "/static/js/main.713fecb5.chunk.js"
+  },
+  {
+    "revision": "b7d6c4b275d0248a64d7",
+    "url": "/static/js/runtime-main.6b219d4b.js"
+  }
+]);
