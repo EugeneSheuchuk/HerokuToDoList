48734191cd49bac7db19be1c79da633860c1a55a
Yauheni Sheuchuk <shevchuked@tut.by>
2020-04-04 13:28:03 +0300

    Refactor code

---
+++ build/precache-manifest.2a9e5dc6ea36987d309dca62b143cabb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d71483965a2c61c170837f20dd07a8d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a61dc5359ef942da87fa",
+    "url": "/static/css/main.4df739ef.chunk.css"
+  },
+  {
+    "revision": "fd4a9af75d32712cc559",
+    "url": "/static/js/2.a864d4b3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.a864d4b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61dc5359ef942da87fa",
+    "url": "/static/js/main.2b822d7f.chunk.js"
+  },
+  {
+    "revision": "b7d6c4b275d0248a64d7",
+    "url": "/static/js/runtime-main.6b219d4b.js"
+  }
+]);
---
+++ build/precache-manifest.697540773339ca25ed52e96db69363d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e365b923b92d74c9c81413d44d25fbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b27a8c91353bb72b6035",
+    "url": "/static/css/main.4df739ef.chunk.css"
+  },
+  {
+    "revision": "fd4a9af75d32712cc559",
+    "url": "/static/js/2.a864d4b3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.a864d4b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b27a8c91353bb72b6035",
+    "url": "/static/js/main.b084b5a5.chunk.js"
+  },
+  {
+    "revision": "b7d6c4b275d0248a64d7",
+    "url": "/static/js/runtime-main.6b219d4b.js"
+  }
+]);
